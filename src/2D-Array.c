55c68a4982108b0186e2eff1b8ac16aad7af3118
vhaegar1526 <dibya.secret@gmail.com>
2021-02-08 23:41:55 +0530

    tic-tac-toe project.

---
+++ src/2D-Array.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char** createBoard(int boardSize){ 
+    // Returns n*n dynamically alloted array.
+    // This 2D array will be storing the state of the board.
+
+    // Create an array of character pointers, each pointer will represent a row.
+    char **board = (char**)malloc(sizeof(char)*boardSize); 
+    
+    // Assign a row to each pointer in array.
+    for(int i=0; i<boardSize; i++)
+        board[i] = (char*)malloc(sizeof(char)*boardSize);
+
+    // Return the freshly baked board.
+    return board;
+}
+
+void labelBoard(char** board, int boardSize){
+    char currLabel = 'A';
+
+    for(int i=0; i<boardSize; i++)
+        for(int j=0; j<boardSize; j++)
+            board[i][j] = currLabel++;
+}
+
+void displayBoard(char** board, int boardSize){
+    for(int i=0; i<boardSize; i++){
+        printf("%c", board[i][0]);
+        for(int j=0; j<boardSize; j++)
+            printf(" %c ", board[i][j]);
+        printf("\n");
+    }
+}
+
+int main(void){
+
+    int boardSize = 0;
+    printf("Creating board.\n");/**/
+    char** board = createBoard(boardSize);
+    printf("Sizeof board -> %lu\n", sizeof(board[0]));/**/
+    labelBoard(board, boardSize);
+    displayBoard(board, boardSize);
+    free(board);
+    return 0;
+}
